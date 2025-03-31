1aa84b49a0ed01d53deeae845881662b1f97b71b
lunacrafts <lunacrafts@protonmail.com>
2025-03-31 14:03:29 +0200

    mirrorboards/mirrorboards

--- init.sh
+++ init.sh
@@ -3,8 +3,8 @@
 # Create mirrorboards directory in home folder if it doesn't exist
 mkdir -p "$HOME/mirrorboards"
 
-# Copy the .mirror.toml file to the created directory
-cp ./maps/.mirror.toml "$HOME/mirrorboards"
+# Download the .mirror.toml file from GitHub to the created directory
+curl -s https://raw.githubusercontent.com/mirrorboards/init/refs/heads/main/maps/.mirror.toml -o "$HOME/mirrorboards/.mirror.toml"
 
 # Change to the mirrorboards directory and run mctl clone command
 cd "$HOME/mirrorboards" && mctl clone .mirror.toml
