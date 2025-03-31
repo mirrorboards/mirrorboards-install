905a16db2888c8bb505da17439a49c5a35ff6bd4
lunacrafts <lunacrafts@protonmail.com>
2025-03-31 22:14:16 +0200

    mirrorboards/mirrorboards

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# Run the install scripts in order
-./install-mctl.sh
-./install-github-mirror.sh
-./install-mount-fish-functions.sh
 
-echo "Installation completed successfully!"
+cd ./mirrorboards/mirrorboards-utils-init
+
+echo $(pwd)
+# ./mirrorboards/mirrorboards-utils-init/install-github-mirror
+# ./mirrorboards/mirrorboards-utils-init/install-fish-functions
+
+# # Run the install scripts in order
+# ./install-mctl.sh
+# ./install-github-mirror.sh
+# ./install-mount-fish-functions.sh
+
+# echo "Installation completed successfully!"
