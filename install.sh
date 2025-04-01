b6bedd73b14d8e97c7404de873d003d57735f2cd
lunacrafts <103828231+lunacrafts@users.noreply.github.com>
2025-04-01 11:02:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/mirrorboards/mirrorboards-install/refs/heads/main/install-mctl-remote.sh)"
 
-mkdir -p "$HOME/mirrorboardsx"
-curl -o "$HOME/mirrorboardsx/mirror.toml" https://raw.githubusercontent.com/mirrorboards/mirrorboards-install/refs/heads/main/maps/mirror.toml
+mkdir -p "$HOME/mirrorboards"
+curl -o "$HOME/mirrorboards/mirror.toml" https://raw.githubusercontent.com/mirrorboards/mirrorboards-install/refs/heads/main/maps/mirror.toml
 
-cd "$HOME/mirrorboardsx"
+cd "$HOME/mirrorboards"
 
 mctl clone mirror.toml
 
